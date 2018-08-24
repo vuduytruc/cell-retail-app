003f770704c5008700d66ae3cb876e539bde3d46
vuduytruc <truc193204@gmail.com>
2018-08-24 15:09:22 +0700

    Revert "19 restructuring code (#21)" (#22)
    This reverts commit 6521cbf8f87651486d45c95154dc5356cce0dc10.

--- server.js
+++ server.js
@@ -3,29 +3,25 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 
 // Get our API routes
-const api = require('./routes/api');
+const api = require('./server/routes/api');
 
 const app = express();
 
-//Middleware for CORS
-app.use(cors());
-
 // Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Set our api routes
 app.use('/api', api);
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 /**
@@ -42,5 +38,5 @@ const server = http.createServer(app);
 /**
  * Listen on provided port, on all network interfaces.
  */
-server.listen(port, () => console.log(`Starting the server at port ${port}`));
+server.listen(port, () => console.log(`API running on localhost:${port}`));
 
